44f8860d60f793605de6e5d62fbc6ac80d4ffb51
Joshua Choy <69736195+jsh-choy@users.noreply.github.com>
2024-04-26 20:35:48 -0400

    Add Weather API Key to fetch temperature and UV light index

---
+++ backend/api/app.js
@@ -0,0 +1,29 @@
+// Fetch weather data
+function fetchWeather(lat, lon, apiKey) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        displayWeather(data);
+        fetchUVIndex(lat, lon, apiKey);
+    })
+    .catch(error => {
+        // Check if the weather is fetching
+        console.error('Error fetching weather:', error);
+        alert("Failed to fetch weather data.");
+    });
+}
+
+// Fetch UV index
+function fetchUVIndex(lat, lon, apiKey) {
+  const uvUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  fetch(uvUrl)
+      .then(response => response.json())
+      .then(data => {
+          displayUVIndex(data);
+      })
+      .catch(error => {
+        console.error('Error fetching UV Index:', error);
+        alert("Failed to fetch UV Index.");
+      });
+}
